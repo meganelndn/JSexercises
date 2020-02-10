f8d47620d42c81375af433e608d0c6050d5b87a8
megane <megane.londono@hotmail.com>
2020-02-10 11:20:37 +0100

    basic variable exercise

---
+++ basics.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const name = "Albus Percival Wulfric Brian Dumbledore";
+const len = name.length;
+const letter = name[0];
+
+console.log(`${name} is ${len} characters long.`)
+console.log(`The second character of ${name} is ${name[3]}`);
+console.log(`The character at index 6 is ${letter[6]}`);
