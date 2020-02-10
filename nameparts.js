65070ef9dac52802abb1a9fe74a0e945f5cc2f0d
megane <megane.londono@hotmail.com>
2020-02-10 11:18:59 +0100

    substring exercise finalized

---
+++ nameparts.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const str = "Mégane Londoño Hermann"
+const firstName = str.substring(0, 6);
+const middleName = str.substring(7, 14);
+const lastName = str.substring(15, 22);
+
+console.log(firstName);
+console.log(middleName);
+console.log(lastName);
