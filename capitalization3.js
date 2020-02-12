e0702125cc982cb6320afc4a9ec81c2afd61beff
megane <megane.londono@hotmail.com>
2020-02-12 10:47:37 +0100

    basics.html

---
+++ capitalization3.js
@@ -0,0 +1,7 @@
+"use strict";
+
+const name = "Mégane";
+const nameOne = "méGane";
+
+console.log(nameOne.toUpperCase(1, 3));
+console.log(nameOne.substring(1, 3));
--- nameparts.js
+++ nameparts.js
@@ -1,9 +1,19 @@
 "use strict";
 
-const str = "Mégane Londoño Hermann"
-const firstName = str.substring(0, 6);
-const middleName = str.substring(7, 14);
-const lastName = str.substring(15, 22);
+const name = "Mégane Londoño Hermann"
+// const firstName = name.substring(0, 6);
+// const middleName = name.substring(7, 14);
+// const lastName = name.substring(15, 22);
+
+const firstSpace = name.indexOf(" ");
+const lastSpace = name.indexOf(" ", (firstSpace + 1));
+
+console.log(firstSpace);
+console.log(lastSpace);
+
+const firstName = name.substring(0, firstSpace);
+const middleName = name.substring(firstSpace, lastSpace);
+const lastName = name.substring(lastSpace, );
 
 console.log(firstName);
 console.log(middleName);
