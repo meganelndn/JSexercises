478f1794652e71dcb1fa93260097fe24de849603
megane <megane.londono@hotmail.com>
2020-02-16 18:04:10 +0100

    converted character in name to lower/upper case

--- capitalization3.js
+++ capitalization3.js
@@ -3,5 +3,20 @@
 const name = "Mégane";
 const nameOne = "méGane";
 
-console.log(nameOne.toUpperCase(1, 3));
-console.log(nameOne.substring(1, 3));
+//3rd letter toUpperCase
+const letter3 = name.substring(2, 3);
+const newLetter = letter3.toUpperCase();
+
+//random combo
+console.log(name.substring(0, 2) + newLetter + name.substring(3, ))
+
+//OR
+console.log(name.substring(0, 2) + name.substring(2, 3).toUpperCase() + name.substring(3, ))
+
+
+//toLowerCase
+console.log(name.substring(0, 2).toUpperCase() + name.substring(2, 4).toLowerCase() + name.substring(4, ).toUpperCase())
+
+
+//modify 1st letter only
+console.log(name.substring(0, 1).toUpperCase() + name.substring(1, ).toLowerCase())
