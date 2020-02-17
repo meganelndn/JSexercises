1367f45574039905ff066ebeda2e329b9995b79f
megane <megane.londono@hotmail.com>
2020-02-17 18:06:57 +0100

    added last comments

--- Animal base/animals_objects.js
+++ Animal base/animals_objects.js
@@ -2,6 +2,13 @@
 window.addEventListener("DOMContentLoaded", init);
 
 const allAnimals = [];
+// 1. Create an object prototype 
+const Animal = {
+    name: "",
+    type: "unknown",
+    desc: "",
+    age: 0
+}
 
 function init() {
     console.log("ready");
@@ -20,9 +27,29 @@ function loadJSON() {
 
 function prepareObjects(jsonData) {
     jsonData.forEach(jsonObject => {
-        // TODO: Create new object with cleaned data - and store that in the allAnimals array
+        // 2. Create a new object from the prototype
+        const animal = Object.create(Animal);
 
-        // TODO: MISSING CODE HERE !!!
+        // 3. Separate the values to assign them to their properties 
+        const firstSpace = jsonObject.fullname.indexOf(" ");
+        const secondSpace = jsonObject.fullname.indexOf(" ", firstSpace + 1);
+        const thirdSpace = jsonObject.fullname.indexOf(" ", secondSpace + 1);
+
+        // 4. Find the values for name, desc, and type from the jsonObject
+        animal.name = jsonObject.fullname.substring(0, firstSpace);
+        //console.log(animal.name)
+        animal.age = jsonObject.age;
+        //console.log(animal.age)
+        animal.desc = jsonObject.fullname.substring(secondSpace + 1, thirdSpace);
+        //console.log(animal.desc)
+        animal.type = jsonObject.fullname.substring(thirdSpace + 1);
+        //console.log(animal.type)
+
+        console.log(animal)
+
+        // 5. Add the new object to the array of animals
+        allAnimals.push(animal);
+        console.log(allAnimals)
     });
 
     displayList();
