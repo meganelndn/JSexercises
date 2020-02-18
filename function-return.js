79a6aa09d755457819b29db140c728da145c1a15
megane <megane.londono@hotmail.com>
2020-02-18 11:18:53 +0100

    a function with multiple return values

--- function-return.js
+++ function-return.js
@@ -22,18 +22,45 @@ function randomBackground() {
     document.querySelector("body").style.backgroundColor = randomColor();
 }
 
-// a function with multiple return values
+// a function with multiple return values: in-class exercise (color palette)
 function hex2rgb(hexcolor) {
 
-    const r = ;
-    const g = ;
-    const b = ;
+    const r;
+    const g;
+    const b;
 
-    const rgb = {
-        "r": r,
-        "g": g,
-        "b": b
+    // const rgb = {
+    //     "r": r,
+    //     "g": g,
+    //     "b": b
+    // };
+
+    // instead of making an object like above, return the consts
+    return {
+        r,
+        g,
+        b
     };
+}
 
-    return rgb
+function anotherFunction() {
+
+    const rgb = hex2rgb("#c0ffee");
+
+    const red = rgb.r;
+    const green = rgb.g;
+}
+
+function convertRBGtoHSL(rgb) {
+
+    const r = rgb.r;
+    const g = rgb.g;
+    const b = rgb.b;
+
+    // instead of sending it to a new value to get the HSL value, return the converted HSL value!
+    return {
+        h,
+        s,
+        l
+    };
 }
