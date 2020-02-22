8b9dd2caeff3243a01970c4a66a08f922551f1db
megane <megane.londono@hotmail.com>
2020-02-22 09:47:14 +0100

    animals chart with filter buttons

---
+++ Animal base/animalbase.js
@@ -0,0 +1,81 @@
+"use strict";
+
+window.addEventListener("DOMContentLoaded", start);
+
+let allAnimals = [];
+
+// The prototype for all animals: 
+const Animal = {
+    name: "",
+    desc: "-unknown animal-",
+    type: "",
+    age: 0
+};
+
+function start() {
+    console.log("ready");
+
+    // TODO: Add event-listeners to filter and sort buttons
+    loadJSON();
+    filterList();
+}
+
+function loadJSON() {
+    fetch("animals.json")
+        .then(response => response.json())
+        .then(jsonData => {
+            // when loaded, prepare objects
+            prepareObjects(jsonData);
+        });
+}
+
+function prepareObjects(jsonData) {
+    allAnimals = jsonData.map(preapareObject);
+
+    // TODO: This might not be the function we want to call first
+    displayList(allAnimals);
+}
+
+function preapareObject(jsonObject) {
+    const animal = Object.create(Animal);
+
+    const texts = jsonObject.fullname.split(" ");
+    animal.name = texts[0];
+    animal.desc = texts[2];
+    animal.type = texts[3];
+    animal.age = jsonObject.age;
+
+    return animal;
+}
+
+// Add function to filter list
+function filterList(animal) {
+    console.log(animal)
+}
+
+function displayList(animals) {
+    // clear the list
+    document.querySelector("#list tbody").innerHTML = "";
+
+    // build a new list
+    animals.forEach(displayAnimal);
+    console.log(allAnimals);
+    animals.forEach(displayCats);
+    animals.forEach(displayDogs);
+
+    //return animals;
+}
+
+function displayAnimal(animal) {
+    // create clone
+    const clone = document.querySelector("template#animal").content.cloneNode(true);
+
+    // set clone data
+    clone.querySelector("[data-field=name]").textContent = animal.name;
+    clone.querySelector("[data-field=desc]").textContent = animal.desc;
+    clone.querySelector("[data-field=type]").textContent = animal.type;
+    clone.querySelector("[data-field=age]").textContent = animal.age;
+
+    // append clone to list
+    document.querySelector("#list tbody").appendChild(clone);
+}
